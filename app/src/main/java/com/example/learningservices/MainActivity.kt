3c8c70ff5c77e1334c1b1dac623aa755c7de037f
LELIK <b_banni@inbox.ru>
2026-03-25 20:45:30 +0300

    Adding a Foreground Service and adding service stops

--- app/src/main/java/com/example/learningservices/MainActivity.kt
+++ app/src/main/java/com/example/learningservices/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.learningservices.databinding.ActivityMainBinding
@@ -23,46 +24,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.simpleService.setOnClickListener {
+            // Функция для остановки сервиса снаружи сервиса
+            stopService(MyForegroundService.newIntent(this))
             startService(MyService.newIntent(this))
         }
         binding.foregroundService.setOnClickListener {
-            requestNotificationPermission()
+            ContextCompat.startForegroundService(
+                this,
+                MyForegroundService.newIntent(this))
         }
-    }
-
-    // Метод если у пользователя отключены уведомления, то откроются настроки для
-    // разрешения уведомлений из приложения
-    // Если уведомления включены, то уцведомление появится на экране
-    private fun requestNotificationPermission() {
-        if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
-            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
-                .putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
-            startActivity(intent)
-        } else {
-            showNotification()
-        }
-    }
-
-    private fun showNotification() {
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                CHANNEL_ID,
-                CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(notificationChannel)
-        }
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Title")
-            .setContentText("Text")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .build()
-        notificationManager.notify(1, notification)
-    }
-
-    companion object {
-        private const val CHANNEL_ID = "channel_id"
-        private const val CHANNEL_NAME = "channel_name"
     }
 }
